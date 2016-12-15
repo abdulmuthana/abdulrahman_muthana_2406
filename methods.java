1a15d8311cf4bcf0e1424b44a76303c85233aefa
Abdulrahman Muthana <muthana.abdulrahman@gmail.com>
2016-12-15 13:28:30 -0500

    java methods

---
+++ methods.java
@@ -0,0 +1,23 @@
+1. int place_order(int product_ID, int quantity) 
+
+2. String Store_History(int order_id, date date_ordered, date date_delivered) 
+
+3. int calculate_revenue(int items_sold, int cost) 
+
+4. int total_due(String product, int cost)
+
+5. int calculate_delivery_time(int Store_number, StringCustomer_address)
+
+6. String product_ordered(string product_name,int product_sold)
+
+7. int most_requested(date date_ordered)
+
+8. int least_requested(date date_ordered)
+
+9. String system_accessUsername(String username)
+
+10. String system_accessPassword(String password)
+
+11. int request_perSecond(int orders, date/time date_ordered)
+
+12. String feedback(String lastname, int account_number)
